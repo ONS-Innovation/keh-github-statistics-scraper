aff979270f3c43c21d003e996968d037fad38e4b
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-02-09 17:35:19 +0000

    fix(conc): update tf env vars

--- concourse/scripts/terraform_infra.sh
+++ concourse/scripts/terraform_infra.sh
@@ -10,14 +10,19 @@ aws_account_id=$(echo "$secrets" | jq -r .aws_account_id)
 aws_access_key_id=$(echo "$secrets" | jq -r .aws_access_key_id)
 aws_secret_access_key=$(echo "$secrets" | jq -r .aws_secret_access_key)
 
-lambda_name=$(echo "$secrets" | jq -r .lambda_name)
-env_name=$(echo "$secrets" | jq -r .env_name)
+domain=$(echo "$secrets" | jq -r .domain)
+ecr_repository_name=$(echo "$secrets" | jq -r .ecr_repository_name)
 
 lambda_timeout=$(echo "$secrets" | jq -r .lambda_timeout)
 
 github_app_client_id=$(echo "$secrets" | jq -r .github_app_client_id)
 aws_secret_name=$(echo "$secrets" | jq -r .aws_secret_name)
 github_org=$(echo "$secrets" | jq -r .github_org)
+
+source_bucket=$(echo "$secrets" | jq -r .source_bucket)
+source_key=$(echo "$secrets" | jq -r .source_key)
+batch_size=$(echo "$secrets" | jq -r .batch_size)
+schedule=$(echo "$secrets" | jq -r .schedule)
 
 export AWS_ACCESS_KEY_ID="$aws_access_key_id"
 export AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
@@ -40,11 +45,14 @@ terraform apply \
 	-var "aws_account_id=$aws_account_id" \
 	-var "aws_access_key_id=$aws_access_key_id" \
 	-var "aws_secret_access_key=$aws_secret_access_key" \
-	-var "env_name=$env_name" \
-	-var "lambda_name=${lambda_name}" \
+	-var "domain=$domain" \
+	-var "ecr_repository_name=$ecr_repository_name" \
 	-var "github_app_client_id=$github_app_client_id" \
 	-var "aws_secret_name=$aws_secret_name" \
 	-var "github_org=$github_org" \
-	-var "lambda_version=${tag}" \
-	-var "lambda_timeout=${lambda_timeout}" \
+	-var "source_bucket=$source_bucket" \
+	-var "source_key=$source_key" \
+	-var "batch_size=$batch_size" \
+	-var "schedule=$schedule" \
+	-var "container_ver=${tag}" \
 	-auto-approve
