8833e2c8b98180772e59e78e1225628134903f92
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-02-09 17:18:07 +0000

    feat: add concourse

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -1,14 +1,21 @@
+#!/bin/sh
+
+# shellcheck disable=SC2154,SC3040
+
 set -euo pipefail
 
 export STORAGE_DRIVER=vfs
 export PODMAN_SYSTEMD_UNIT=concourse-task
 
-container_image=$(echo "$secrets" | jq -r .ecr_repository_name)
+container_image=$(echo "$secrets" | jq -r .ecr_repository)
 
-aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
+aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com
 
-podman build -t ${container_image}:${tag} resource-repo
+podman build -t "${container_image}":"${tag}" resource-repo/
 
-podman tag ${container_image}:${tag} ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
+podman tag "${container_image}":"${tag}" "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com/"${container_image}":"${tag}"
 
-podman push ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
+podman push "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com/"${container_image}":"${tag}"
+
+echo "Saving image as tar for next task..."
+podman save --format=oci-dir "${container_image}:${tag}" -o built-images/github_statistics_scraper.tar
