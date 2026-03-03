734e1ae40391167b6e01d3e9e92079ef777ceae9
Cooper Wright <cooper.wright@ons.gov.uk>
2026-03-03 13:49:13 +0000

    fix: added new version of batch tf module, to make root filesystem read only

--- terraform/batch/main.tf
+++ terraform/batch/main.tf
@@ -11,7 +11,7 @@ resource "aws_iam_policy" "s3_access" {
 }
 
 module "batch_eventbridge" {
-  source = "git::https://github.com/ONS-Innovation/keh-scheduled-batch-tf-module.git?ref=v1.1.0"
+  source = "git::https://github.com/ONS-Innovation/keh-scheduled-batch-tf-module.git?ref=v1.1.1"
 
   aws_account_id        = var.aws_account_id
   aws_access_key_id     = var.aws_access_key_id
